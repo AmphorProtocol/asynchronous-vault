4f8a9648843d656dd3690470528b23af5c9e66fc
blablalf <contact@alfred-gaillard.fr>
2023-12-12 08:26:18 +0100

    Renamed contracts folder

---
+++ amphor-contracts/src/AmphorAsyncSynthVaultRequestLPImp.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import {ERC6909} from "ERC-6909/ERC6909.sol";
+
+contract AmphorAsyncSynthVaultRequestLPImp is ERC6909 {
+    constructor() ERC6909() {}
+
+    // TODO: add batched version
+}
