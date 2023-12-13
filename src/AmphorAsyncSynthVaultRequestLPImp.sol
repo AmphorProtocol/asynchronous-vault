4c9308cf9ae16562bc0a2dd2f4b5680441d469c3
blablalf <contact@alfred-gaillard.fr>
2023-12-13 18:05:46 +0100

    Choosing the path of 6909ib

--- src/AmphorAsyncSynthVaultRequestLPImp.sol
+++ src/AmphorAsyncSynthVaultRequestLPImp.sol
@@ -1,17 +1,17 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
-import {ERC6909} from "ERC-6909/ERC6909.sol";
+import {ERC6909ib} from "ERC-6909/ERC6909ib.sol";
 import {
     Ownable,
     Ownable2Step
 } from "@openzeppelin/contracts/access/Ownable2Step.sol";
 
-contract AmphorAsyncSynthVaultRequestLPImp is ERC6909, Ownable2Step {
+contract AmphorAsyncSynthVaultRequestLPImp is ERC6909ib, Ownable2Step {
 
     /// @notice Total supply for a token.
     mapping(uint256 tokenId => uint256 supply) public totalSupply;
-    constructor() ERC6909() Ownable(_msgSender()) {}
+    constructor() ERC6909ib("amphorPendingDepositLP", "aDep") Ownable(_msgSender()) {}
     uint256 public constant MAX_UINT256 = type(uint256).max;
 
     function mint(
@@ -45,27 +45,27 @@ contract AmphorAsyncSynthVaultRequestLPImp is ERC6909, Ownable2Step {
         emit Transfer(msg.sender, sender, address(0), id, amount);
     }
 
-    function getPositiveBalances(address account, uint256 epochNonce)
-        external
-        view
-        returns (uint256[] memory ids, uint256[] memory positiveBalances)
-    {
-        uint256[] memory allBalances = new uint256[](epochNonce);
-        uint256[] memory allIds = new uint256[](epochNonce);
-        uint256 allBalancesIndex;
-        for (allBalancesIndex; allBalancesIndex < epochNonce; allBalancesIndex++) {
-            uint256 lpBalances = balanceOf[account][allBalancesIndex];
-            if (lpBalances > 0) {
-                allBalances[allBalancesIndex] = lpBalances;
-                allIds[allBalancesIndex] = allBalancesIndex;
-            }
-        }
-        positiveBalances = new uint256[](allBalancesIndex);
-        ids = new uint256[](allBalancesIndex);
-        uint256 positiveBalancesIndex;
-        for (positiveBalancesIndex; positiveBalancesIndex < allBalancesIndex; positiveBalancesIndex++) {
-            positiveBalances[positiveBalancesIndex] = positiveBalances[positiveBalancesIndex];
-            ids[positiveBalancesIndex] = allIds[positiveBalancesIndex];
-        }
-    } 
+    // function getPositiveBalances(address account, uint256 epochNonce)
+    //     external
+    //     view
+    //     returns (uint256[] memory ids, uint256[] memory positiveBalances)
+    // {
+    //     uint256[] memory allBalances = new uint256[](epochNonce);
+    //     uint256[] memory allIds = new uint256[](epochNonce);
+    //     uint256 allBalancesIndex;
+    //     for (allBalancesIndex; allBalancesIndex < epochNonce; allBalancesIndex++) {
+    //         uint256 lpBalances = balanceOf[account][allBalancesIndex];
+    //         if (lpBalances > 0) {
+    //             allBalances[allBalancesIndex] = lpBalances;
+    //             allIds[allBalancesIndex] = allBalancesIndex;
+    //         }
+    //     }
+    //     positiveBalances = new uint256[](allBalancesIndex);
+    //     ids = new uint256[](allBalancesIndex);
+    //     uint256 positiveBalancesIndex;
+    //     for (positiveBalancesIndex; positiveBalancesIndex < allBalancesIndex; positiveBalancesIndex++) {
+    //         positiveBalances[positiveBalancesIndex] = positiveBalances[positiveBalancesIndex];
+    //         ids[positiveBalancesIndex] = allIds[positiveBalancesIndex];
+    //     }
+    // } 
 }
