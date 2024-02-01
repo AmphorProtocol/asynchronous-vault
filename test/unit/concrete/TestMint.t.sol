a05023f1d8f4c8d9aef789e8f48711696ca80975
blablalf <contact@alfred-gaillard.fr>
2024-02-01 18:49:10 +0100

    deposit/mint/withdraw/redeem btt first iteration

--- test/unit/concrete/TestMint.t.sol
+++ test/unit/concrete/TestMint.t.sol
@@ -2,71 +2,71 @@
 pragma solidity 0.8.21;
 
 contract TestMint {
-    function test_WhenIsClosed() external {
+    function test_WhenMintGivenVaultClosed() external {
         // it should revert with ERC4626ExceededMaxMint
     }
 
-    function test_WhenVaultIsOpen() external {
-        // it should not revert
+    function test_WhenMintGivenVaultOpen() external {
+        // it should revert if the requested amount of shares converted in asset is higher than the allowance of the owner to the vault
+        // it should revert if the requested amount of shares converted in asset is higher than the shares balance of the owner
         // it should decrease the underlying balance of the owner
         // it should emit a Deposit event
         // it should increase the underlying balance of the vault
         // it should increase the balance of shares of the receiver
         // it should return the same value as the one returned by previewMint
-        // it should return the same value as the given (to the receiver) shares amount
+        // it should return the same value as the taken (to the owner) underlying amount
         // it should increase the total supply of shares
-        // it should return the same value as the one returned by previewMint
         // it should take the underlying amount returned by previewMint to the owner
     }
 
-    function test_WhenTheReceiverIsNotTheOwner() external {
-        // it should not revert
+    function test_WhenMintGivenReceiverNotEqualOwner() external {
+        // it should revert if the requested amount of shares converted in asset is higher than the allowance of the owner to the vault
+        // it should revert if the requested amount of shares converted in asset is higher than the shares balance of the owner
         // it should decrease the underlying balance of the owner
         // it should emit a Deposit event
         // it should increase the underlying balance of the vault
         // it should increase the balance of shares of the receiver
         // it should return the same value as the one returned by previewMint
-        // it should return the same value as the given (to the receiver) shares amount
+        // it should return the same value as the taken (to the owner) underlying amount
         // it should increase the total supply of shares
-        // it should return the same value as the one returned by previewMint
         // it should take the underlying amount returned by previewMint to the owner
     }
 
-    function test_WhenVaultIsEmpty() external {
-        // it should not revert
+    function test_WhenMintGivenVaultEmpty() external {
+        // it should revert if the requested amount of shares converted in asset is higher than the allowance of the owner to the vault
+        // it should revert if the requested amount of shares converted in asset is higher than the shares balance of the owner
         // it should decrease the underlying balance of the owner
         // it should emit a Deposit event
         // it should increase the underlying balance of the vault
         // it should increase the balance of shares of the receiver
         // it should return the same value as the one returned by previewMint
-        // it should return the same value as the given (to the receiver) shares amount
+        // it should return the same value as the taken (to the owner) underlying amount
         // it should increase the total supply of shares
-        // it should return the same value as the one returned by previewMint
         // it should take the underlying amount returned by previewMint to the owner
     }
 
-    function test_WhenTheDepositAmountIs0() external {
-        // it should not revert
-        // it should decrease the underlying balance of the owner
+    function test_WhenMintGivenDepositAmountIs0() external {
+        // it should revert if the requested amount of shares converted in asset is higher than the allowance of the owner to the vault
+        // it should revert if the requested amount of shares converted in asset is higher than the shares balance of the owner
+        // it should nor increase and decrease the underlying balance of the owner
         // it should emit a Deposit event
-        // it should increase the underlying balance of the vault
-        // it should increase the balance of shares of the receiver
+        // it should nor increase and decrease the underlying balance of the vault
+        // it should nor increase and decrease the balance of shares of the receiver
         // it should return the same value as the one returned by previewMint
-        // it should return the same value as the given (to the receiver) shares amount
-        // it should increase the total supply of shares
-        // it should return the same value as the one returned by previewMint
+        // it should return the same value as the taken (to the owner) underlying amount
+        // it should nor increase and decrease the total supply of shares
         // it should take the underlying amount returned by previewMint to the owner
     }
 
-    function test_WhenVaultIsOpenAndPaused() external {
+    function test_WhenMintGivenVaultOpenGivenVaultPaused() external {
         // it should revert with ERC4626ExceededMaxDeposit
     }
 
-    function test_WhenVaultIsClosedAndPaused() external {
+    function test_WhenMintGivenVaultClosedGivenVaultPaused() external {
         // it should revert with ERC4626ExceededMaxDeposit
     }
 
-    function test_WhenReceiverIsAddress0() external {
+    function test_WhenMintGivenReceiverIsAddress0() external {
         // it should revert with ERC20InvalidReceiver
     }
 }
