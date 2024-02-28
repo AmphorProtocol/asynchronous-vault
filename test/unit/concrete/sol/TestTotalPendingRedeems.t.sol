530335c9459ac8ba2edafb6007b16ee8695819a4
gdupont <gdupont.pro@gmail.com>
2024-02-28 14:15:27 +0100

    test are working

--- test/unit/concrete/sol/TestTotalPendingRedeems.t.sol
+++ test/unit/concrete/sol/TestTotalPendingRedeems.t.sol
@@ -3,12 +3,29 @@ pragma solidity 0.8.21;
 
 import { TestBase } from "../../../Base.t.sol";
 
-contract TestTotalPendingRedeems {
+contract TestTotalPendingRedeems is TestBase {
     function test_GivenVaultOpenWhenTotalPendingRedeems() external {
         // it should return 0
+        usersDealApproveAndDeposit(1);
+        assertClose(vaultUSDC);
+        assertRequestRedeem(
+            vaultUSDC, user1.addr, user1.addr, user1.addr, 100, ""
+        );
+        assertOpen(vaultUSDC, 0);
+        assertEq(
+            vaultUSDC.totalPendingRedeems(), 0, "Invalid total pending redeem"
+        );
     }
 
     function test_GivenVaultClosedWhenTotalPendingRedeems() external {
-        // it should return shares balance of vault
+        // it should return underlying balance of the silo contract
+        usersDealApproveAndDeposit(1);
+        assertClose(vaultUSDC);
+        assertRequestRedeem(
+            vaultUSDC, user1.addr, user1.addr, user1.addr, 100, ""
+        );
+        assertEq(
+            vaultUSDC.totalPendingRedeems(), 100, "Invalid total pending redeem"
+        );
     }
 }
