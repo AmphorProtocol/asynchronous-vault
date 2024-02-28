bbc5c19000ec5980a4c0a0cd2616dcef379a0152
blablalf <contact@alfred-gaillard.fr>
2024-02-28 17:35:33 +0100

    add TestTotalPendingRedeems

--- test/unit/concrete/sol/TestTotalPendingRedeems.t.sol
+++ test/unit/concrete/sol/TestTotalPendingRedeems.t.sol
@@ -3,12 +3,23 @@ pragma solidity 0.8.21;
 
 import { TestBase } from "../../../Base.t.sol";
 
-contract TestTotalPendingRedeems {
+contract TestTotalPendingRedeems is TestBase {
+    function setUp() external {
+        usersDealApproveAndDeposit(1);
+    }
+
     function test_GivenVaultOpenWhenTotalPendingRedeems() external {
         // it should return 0
+        closeVaults();
+        requestRedeem(vaultUSDC, user1, 1, "");
+        assertOpen(vaultUSDC, 0);
+        assertEq(vaultUSDC.totalPendingRedeems(), 0);
     }
 
     function test_GivenVaultClosedWhenTotalPendingRedeems() external {
         // it should return shares balance of vault
+        closeVaults();
+        requestRedeem(vaultUSDC, user1, 1, "");
+        assertEq(vaultUSDC.totalPendingRedeems(), 1);
     }
 }
