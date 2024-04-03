b0130ee9fad53bcb412879c3f841aee7188d3bed
blablalf <contact@alfred-gaillard.fr>
2024-04-03 14:20:44 +0200

    Added zapper

--- script/mainnet_deploy.s.sol
+++ script/mainnet_deploy.s.sol
@@ -10,7 +10,7 @@ import { UpgradeableBeacon } from
 import { BeaconProxy } from
     "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
 
-contract MAINNET_DeployAmphorSynthetic is Script {
+contract MAINNET_DeployAmphor is Script {
     uint256 privateKey;
     uint16 fees;
     string vaultNameUSDC;
@@ -20,6 +20,7 @@ contract MAINNET_DeployAmphorSynthetic is Script {
     address owner;
     address usdcAddr;
     address wethAddr;
+    address amphor;
     uint256 bootstrapUSDC;
     uint256 bootstrapWETH;
     uint256 nonce; // beacon deployment + approve
@@ -30,7 +31,7 @@ contract MAINNET_DeployAmphorSynthetic is Script {
         // uncomment the following line
         privateKey = vm.envUint("PRIVATE_KEY");
 
-        owner = vm.envAddress("AMPHORLABS_ADDRESS");
+        amphor = vm.envAddress("AMPHORLABS_ADDRESS");
         owner = vm.addr(privateKey);
         fees = uint16(vm.envUint("INITIAL_FEES_AMOUNT"));
         vaultNameUSDC = vm.envString("USDC_V1_NAME");
@@ -105,8 +106,8 @@ contract MAINNET_DeployAmphorSynthetic is Script {
         vm.stopBroadcast();
 
         // Mainnet 
-        // source .env && forge clean && forge script script/mainnet_deploy.s.sol:MAINNET_DeployAmphorSynthetic --ffi --chain-id 1 --optimizer-runs 10000 --verifier-url ${VERIFIER_URL} --etherscan-api-key ${ETHERSCAN_API_KEY} --verify #--broadcast
+        // source .env && forge clean && forge script script/mainnet_deploy.s.sol:MAINNET_DeployAmphor --ffi --chain-id 1 --optimizer-runs 10000 --verifier-url ${VERIFIER_URL} --etherscan-api-key ${ETHERSCAN_API_KEY} --verify #--broadcast
         // Sepolia
-        // source .env && forge clean && forge script script/mainnet_deploy.s.sol:MAINNET_DeployAmphorSynthetic --ffi --chain-id 534351 --optimizer-runs 10000 --verifier-url ${VERIFIER_URL_SEPOLIA} --etherscan-api-key ${ETHERSCAN_API_KEY} --verify #--broadcast
+        // source .env && forge clean && forge script script/mainnet_deploy.s.sol:MAINNET_DeployAmphor --ffi --chain-id 534351 --optimizer-runs 10000 --verifier-url ${VERIFIER_URL_SEPOLIA} --etherscan-api-key ${ETHERSCAN_API_KEY} --verify #--broadcast
     }
 }
